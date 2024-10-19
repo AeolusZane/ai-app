86b700ea7146bdba6d849752d2a24dbef6b70110
AeolusZane <1393649219@qq.com>
2024-10-19 18:06:50 +0800

    db connect

---
+++ utils/db.ts
@@ -0,0 +1,14 @@
+/**
+ * 防止 Prisma 在每次请求时都创建新的数据库连接
+ */
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+    log: ["query"],
+});
+
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
+}
